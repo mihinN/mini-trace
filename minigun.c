56352e1ff1af6689ac327b02b059d5a8482160ad
mihinN <anonnimnaka@gmail.com>
2024-04-05 23:11:45 +0530

    Updated

--- minigun.c
+++ minigun.c
@@ -9,8 +9,36 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-int do_child(int argc, char** argv);
-int do_trace(pid_t child);
+int do_child(int argc, char** argv) {
+        char* args[argc + 1];
+        memcpy(args, argv, argc * sizeof(char*));
+        args[argc] = NULL;
+
+        ptrace(PTRACE_TRACEME);
+
+        kill(getpid(), SIGSTOP);
+        return execvp(args[0], args);
+}
+
+int wait_for_syscall(pid_t child);
+
+int do_trace(pid_t child) {
+        int status, syscall, retval;
+        waitpid(child , &status, 0);
+        ptrace(PTRACE_SETOPTIONS, child, 0 , PTRACE_O_TRACESYSGOOD);
+
+                while(1) {
+                        if (wait_for_syscall(child) != 0);
+                        syscall = ptrace(PTRACE_PEEKUSER, child, sizeof(long)* ORIG_RAX);
+                        fprintf(stderr, "syscall(%d) = ", syscall);
+                        if (wait_for_syscall(child) != 0) break;
+                        retval = ptrace((PTRACE_PEEKUSER), child, sizeof(long)*RAX);
+                        fprintf(stderr, "%d\n", retval);
+                }
+
+                return 0;
+        
+}
 
 int main(int argc , char** argv) {
 
